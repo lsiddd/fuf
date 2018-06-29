b8bcd89eb95b4b90f1f4222547ae2b0ccc0e04ac
lsiddd <lucassidpacheco@gmail.com>
2018-06-29 15:15:42 -0300

    add suport to all datasets included

--- reader.m
+++ reader.m
@@ -1,10 +1,18 @@
+%inicializzação das matrizes
 X = [];
 D = [];
+
+%iterar nos arquivos de imagem
 for i = [0:9]
-  directory = sprintf("*/0001_CH4M/%d/*png", i);
+  directory = sprintf("*/0010_CH3M/%d/*png", i);
   j = dir(directory);
   for k = j'
-    X = [X; imread(sprintf("%s/%s", k.folder, k.name))(1:9800)];
+    try
+      image = double(rgb2gray(imread(sprintf("%s/%s", k.folder, k.name))))(1:8100);
+    catch exception
+      image = imread(sprintf("%s/%s", k.folder, k.name))(1:8100);
+    end_try_catch
+  X = [X;image];
     d = [0 0 0 0 0 0 0 0 0 0];
     d(i + 1) = 1;
     D = [D; d];
